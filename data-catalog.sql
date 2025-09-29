27b6d92884f4ac6b982a439ca108aa354e4f8371
olgabogdanova-lgtm <olga.bogdanova@datavirtuality.de>
2025-09-29 17:02:13 +0200

    Create data-catalog.sql

---
+++ data-catalog.sql
@@ -0,0 +1,2 @@
+CALL SYSADMIN.setDataCatalogAttribute(name => 'oracle.test_a.a', attribute => 'a', attrValue => '4') OPTION $NOFAIL;;
+CALL SYSADMIN.setDataCatalogAttribute(name => 'oracle.test_a.b', attribute => 'b', attrValue => '4') OPTION $NOFAIL;;
